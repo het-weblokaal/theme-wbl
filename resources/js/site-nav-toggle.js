5bdadf0a85b8f004e63741be091e01b632375a52
Erik Joling <erik@ejoweb.nl>
2020-11-18 09:36:09 +0100

    Fix minor site-nav-toggle issue

--- resources/js/site-nav-toggle.js
+++ resources/js/site-nav-toggle.js
@@ -6,114 +6,114 @@
 
 var siteNavToggle = (function() {
 
-    var props   = {}; // Store properties
-    var methods = {}; // Store methods (functions)
-    var elems   = {}; // Store elements
-    var events  = {}; // Store events
-    var helpers = {}; // Store private helper methods
+	var props   = {}; // Store properties
+	var methods = {}; // Store methods (functions)
+	var elems   = {}; // Store elements
+	var events  = {}; // Store events
+	var helpers = {}; // Store private helper methods
 
-    /* ENGINE
-    -------------------------------------*/
+	/* ENGINE
+	-------------------------------------*/
 
-    // Initialize
-    methods.init = function( customOptions ) {
+	// Initialize
+	methods.init = function( customOptions ) {
 
-        // Setup toggle and target
-        methods.setup( customOptions );
+		// Setup toggle and target
+		methods.setup( customOptions );
 
-        // We only need to control if we have a toggle and a target
-        if ( elems.toggle && elems.target ) {
+		// We only need to control if we have a toggle and a target
+		if ( elems.toggle && elems.target ) {
 
-            // Make sure toggle and target are in the right state
-            methods.reset();
+			// Make sure toggle and target are in the right state
+			methods.reset();
 
-            // Manage control of toggle and target
-            methods.control();
-        }
-    };
+			// Manage control of toggle and target
+			methods.control();
+		}
+	};
 
-    // Setup
-    methods.setup = function( customOptions ) {
+	// Setup
+	methods.setup = function( customOptions ) {
 
 		// Set default options
 		props.options = {
-            rootSelector:   'body',
-            toggleSelector: '.site-nav-toggle',
-            targetSelector: '.site-nav',
-            activeRootClass: 'has-expanded-site-nav'
+			rootSelector:   'body',
+			toggleSelector: '.site-nav-toggle',
+			targetSelector: '.site-nav',
+			activeRootClass: 'has-expanded-site-nav'
 		};
 
-        // Merge options
-        props.options = helpers.extend( true, props.options, customOptions );
+		// Merge options
+		props.options = helpers.extend( true, props.options, customOptions );
 
-        // Get root
-        elems.root = document.querySelector( props.options.rootSelector );
+		// Get root
+		elems.root = document.querySelector( props.options.rootSelector );
 
-        // Get toggle
-        elems.toggle = document.querySelector( props.options.toggleSelector );
+		// Get toggle
+		elems.toggle = document.querySelector( props.options.toggleSelector );
 
-        // Get navigation based on toggle-target
-        elems.target = document.querySelector( props.options.targetSelector );
+		// Get navigation based on toggle-target
+		elems.target = document.querySelector( props.options.targetSelector );
 
-        if ( elems.target ) {
+		if ( elems.target ) {
 
-            // Focusable elements inside the target
-            elems.targetFocusable = elems.target.querySelectorAll([
-                'a[href]',
-                'area[href]',
-                'input:not([disabled])',
-                'select:not([disabled])',
-                'textarea:not([disabled])',
-                'button:not([disabled])',
-                'iframe',
-                'object',
-                'embed',
-                '[contenteditable]',
-                '[tabindex]:not([tabindex^="-"])'
-            ]);
-        }
-    };
+			// Focusable elements inside the target
+			elems.targetFocusable = elems.target.querySelectorAll([
+				'a[href]',
+				'area[href]',
+				'input:not([disabled])',
+				'select:not([disabled])',
+				'textarea:not([disabled])',
+				'button:not([disabled])',
+				'iframe',
+				'object',
+				'embed',
+				'[contenteditable]',
+				'[tabindex]:not([tabindex^="-"])'
+			]);
+		}
+	};
 
-    // Control
-    methods.control = function() {
+	// Control
+	methods.control = function() {
 
-        // Manage opening and closing of target
-        events.onClickToggle( methods.toggle );
+		// Manage opening and closing of target
+		events.onClickToggle( methods.toggle );
 
-        // // Extra closing methods
-        events.onPressEscape( methods.close );
-        events.onClickOutsideTarget( methods.close );
-    };
+		// // Extra closing methods
+		events.onPressEscape( methods.close );
+		events.onClickOutsideTarget( methods.close );
+	};
 
-    /**
-     * Make sure toggle and target are in the right state
-     */
-    methods.reset = function() {
-        elems.toggle.setAttribute('aria-expanded', 'false');
-        elems.target.setAttribute('aria-expanded', 'false');
-        elems.root.classList.remove(props.options.activeRootClass);
+	/**
+	 * Make sure toggle and target are in the right state
+	 */
+	methods.reset = function() {
+		elems.toggle.setAttribute('aria-expanded', 'false');
+		elems.target.setAttribute('aria-expanded', 'false');
+		elems.root.classList.remove(props.options.activeRootClass);
 
-        // Force toggle to be focusable
-        // elems.toggle.tabIndex = 0;
+		// Force toggle to be focusable
+		// elems.toggle.tabIndex = 0;
 
-        // Prevent target from getting focus
-        methods.preventFocusInTarget();
-    };
+		// Prevent target from getting focus
+		methods.preventFocusInTarget();
+	};
 
-    /* FUNCTIONS
-    -------------------------------------*/
+	/* FUNCTIONS
+	-------------------------------------*/
 
-    /**
+	/**
 	 * Toggle menu classes and ARIA.
 	 */
 	methods.toggle = function( toggle ) {
 
 		if (methods.isOpen()) {
 			methods.close();
-        }
+		}
 		else {
 			methods.open( toggle );
-        }
+		}
 	};
 
 	/**
@@ -121,21 +121,21 @@ var siteNavToggle = (function() {
 	 */
 	methods.open = function( toggle ) {
 
-        // Don't open if target is not closed
-        if ( ! methods.isClose() ) {
-            return;
-        }
+		// Don't open if target is not closed
+		if ( ! methods.isClose() ) {
+			return;
+		}
 
 		elems.toggle.setAttribute('aria-expanded', 'true');
-        elems.target.setAttribute('aria-expanded', 'true');
-        elems.root.classList.add(props.options.activeRootClass);
+		elems.target.setAttribute('aria-expanded', 'true');
+		elems.root.classList.add(props.options.activeRootClass);
 
 		// Get lastFocus element
-        elems.lastFocus = toggle;
+		elems.lastFocus = toggle;
 
-        // Trap focus in target
-        methods.allowFocusInTarget();
-        methods.trapFocusInTarget();
+		// Trap focus in target
+		methods.allowFocusInTarget();
+		methods.trapFocusInTarget();
 	};
 
 	/**
@@ -143,218 +143,221 @@ var siteNavToggle = (function() {
 	 */
 	methods.close = function() {
 
-        // Don't close if target is not open
-        if ( ! methods.isOpen() ) {
-            return;
-        }
+		// Don't close if target is not open
+		if ( ! methods.isOpen() ) {
+			return;
+		}
 
 		elems.toggle.setAttribute('aria-expanded', 'false');
 		elems.target.setAttribute('aria-expanded', 'false');
 		elems.root.classList.remove(props.options.activeRootClass);
 
 		// Return focus to lastFocussed element
-        if (elems.lastFocus) {
-            elems.lastFocus.focus();
-        }
+		if (elems.lastFocus) {
+			elems.lastFocus.focus();
+		}
 
-        // Prevent target from getting focus
-        methods.preventFocusInTarget();
+		// Prevent target from getting focus
+		methods.preventFocusInTarget();
 	};
 
 	/**
 	 * Check if target is open (expanded)
 	 */
 	methods.isOpen = function() {
-        return elems.target.getAttribute('aria-expanded') == 'true';
-    };
+		return elems.target.getAttribute('aria-expanded') == 'true';
+	};
 
-    /**
-     * Check if target is closed (not expanded)
-     */
-    methods.isClose = function() {
-        return ! methods.isOpen();
-    };
+	/**
+	 * Check if target is closed (not expanded)
+	 */
+	methods.isClose = function() {
+		return ! methods.isOpen();
+	};
 
-    // Trap focus in target
-    methods.trapFocusInTarget = function() {
+	// Trap focus in target
+	methods.trapFocusInTarget = function() {
 
-        // Get first and last focusable element to define the edges of the tab-scope
-        const firstFocusableElem = elems.targetFocusable[0];
-        const lastFocusableElem  = elems.targetFocusable[elems.targetFocusable.length - 1];
+		// Get first and last focusable element to define the edges of the tab-scope
+		const firstFocusableElem = elems.targetFocusable[0];
+		const lastFocusableElem  = elems.targetFocusable[elems.targetFocusable.length - 1];
 
-        // On Tab of the toggle
-        elems.toggle.addEventListener('keydown', function (e) {
+		// On Tab of the toggle
+		elems.toggle.addEventListener('keydown', function (e) {
 
-            if ( methods.isOpen() ) {
+			if ( methods.isOpen() ) {
 
-                // Redirect Shift+Tab to lastFocusableElem
-                if (9 === e.keyCode && e.shiftKey) {
-                    e.preventDefault();
-                    lastFocusableElem.focus(); // Set focus on first element.
-                }
+				// Redirect Shift+Tab to lastFocusableElem
+				if (9 === e.keyCode && e.shiftKey) {
+					e.preventDefault();
+					lastFocusableElem.focus(); // Set focus on first element.
+				}
 
-                // Redirect Tab to firstFocusableElem
-                if (9 === e.keyCode && !e.shiftKey) {
-                    e.preventDefault();
-                    firstFocusableElem.focus();
-                }
-            }
-        });
+				// Redirect Tab to firstFocusableElem
+				if (9 === e.keyCode && !e.shiftKey) {
+					e.preventDefault();
+					firstFocusableElem.focus();
+				}
+			}
+		});
 
-        // On Tab of the first target focusable elem
-        firstFocusableElem.addEventListener('keydown', function (e) {
+		// On Tab of the first target focusable elem
+		firstFocusableElem.addEventListener('keydown', function (e) {
 
-            if ( methods.isOpen() ) {
+			if ( methods.isOpen() ) {
 
-                // Redirect first Shift+Tab to toggle
-                if (9 === e.keyCode && e.shiftKey) {
-                    e.preventDefault();
-                    elems.toggle.focus(); // Set focus on first element.
-                }
-            }
-        });
+				// Redirect first Shift+Tab to toggle
+				if (9 === e.keyCode && e.shiftKey) {
+					e.preventDefault();
+					elems.toggle.focus(); // Set focus on first element.
+				}
+			}
+		});
 
-        // On Tab of the last target focusable elem
-        lastFocusableElem.addEventListener('keydown', function (e) {
+		// On Tab of the last target focusable elem
+		lastFocusableElem.addEventListener('keydown', function (e) {
 
-            if ( methods.isOpen() ) {
+			if ( methods.isOpen() ) {
 
-                // Redirect last Tab to toggle
-                if (9 === e.keyCode && !e.shiftKey) {
-                    e.preventDefault();
-                    elems.toggle.focus();
-                }
-            }
-        });
-    };
+				// Redirect last Tab to toggle
+				if (9 === e.keyCode && !e.shiftKey) {
+					e.preventDefault();
+					elems.toggle.focus();
+				}
+			}
+		});
+	};
 
-    // Allow focus in target
-    methods.allowFocusInTarget = function() {
+	// Allow focus in target
+	methods.allowFocusInTarget = function() {
 
-        // Remove negative tabindexes set by methods.preventFocusInTarget()
-        for (var i = 0; i < elems.targetFocusable.length; i++) {
-            elems.targetFocusable[i].removeAttribute('tabindex');
-        }
-    };
+		// Remove negative tabindexes set by methods.preventFocusInTarget()
+		for (var i = 0; i < elems.targetFocusable.length; i++) {
+			elems.targetFocusable[i].removeAttribute('tabindex');
+		}
+	};
 
-    // Prevent focus in target
-    methods.preventFocusInTarget = function() {
+	// Prevent focus in target
+	methods.preventFocusInTarget = function() {
 
-        // Set tabindex to -1 to prevent tabbing
-        for (var i = 0; i < elems.targetFocusable.length; i++) {
-            elems.targetFocusable[i].tabIndex = -1;
-        }
-    };
-
-
-    /* EVENTS
-    -------------------------------------*/
-
-    // On click target toggle
-    events.onClickToggle = function(callback) {
-
-        // Exit if callback is not a function
-        if (typeof callback !== "function")
-            return;
-
-        // Click on toggle
-        document.addEventListener( 'click', function(event) {
-
-            // Check if toggle
-            if ( elems.toggle.contains( event.target ) ) {
-                event.preventDefault();
-
-                callback(event.target);
-            }
-
-        }, false );
-    };
-
-    // On press Escape Key
-    events.onPressEscape = function(callback) {
-
-        // Check if callback is function
-        if (typeof callback !== "function")
-            return;
-
-        document.addEventListener('keyup', function(event) {
-
-            // 27 = Escape key
-            if (27 === event.keyCode) {
-                callback();
-            }
-        }, false);
-    }
-
-    // Click outside target
-    events.onClickOutsideTarget = function( callback ) {
-
-        // Check if callback is function
-        if (typeof callback !== "function")
-            return;
-
-        // Listen to click outside target
-        document.addEventListener( 'click', function( event ) {
-
-            // Only proceed if click is outside target. Toggle is not outside target
-            if ( ! elems.target.contains( event.target ) && ! elems.toggle.contains( event.target ) ) {
-            	console.log('onClickOutsideTarget');
-                callback();
-            }
+		// Set tabindex to -1 to prevent tabbing
+		for (var i = 0; i < elems.targetFocusable.length; i++) {
+			elems.targetFocusable[i].tabIndex = -1;
+		}
+	};
 
 
-        }, false);
-    };
+	/* EVENTS
+	-------------------------------------*/
+
+	// On click target toggle
+	events.onClickToggle = function(callback) {
+
+		// Exit if callback is not a function
+		if (typeof callback !== "function")
+			return;
+
+		// Click on toggle
+		document.addEventListener( 'click', function(event) {
+
+			// Check if toggle
+			if ( elems.toggle.contains( event.target ) ) {
+				event.preventDefault();
+
+				callback(event.target);
+			}
+
+		}, false );
+	};
+
+	// On press Escape Key
+	events.onPressEscape = function(callback) {
+
+		// Check if callback is function
+		if (typeof callback !== "function")
+			return;
+
+		document.addEventListener('keyup', function(event) {
+
+			// 27 = Escape key
+			if (27 === event.keyCode) {
+				callback();
+			}
+		}, false);
+	}
+
+	// Click outside target
+	events.onClickOutsideTarget = function( callback ) {
+
+		// Check if callback is function
+		if (typeof callback !== "function")
+			return;
+
+		// Listen to click outside target
+		document.addEventListener( 'click', function( event ) {
+
+			// Only check if navigation is opened
+			if ( methods.isOpen() ) {
+
+				// Only proceed if click is outside target. Toggle is not outside target
+				if ( ! elems.target.contains( event.target ) && ! elems.toggle.contains( event.target ) ) {
+					callback();
+				}
+			}
 
 
-    /* HELPERS
-    -------------------------------------*/
+		}, false);
+	};
 
-    // Pass in the objects to merge as arguments.
-    // For a deep extend, set the first argument to `true`.
-    helpers.extend = function () {
 
-        // Variables
-        var extended = {};
-        var deep = false;
-        var i = 0;
-        var length = arguments.length;
+	/* HELPERS
+	-------------------------------------*/
 
-        // Check if a deep merge
-        if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
-            deep = arguments[0];
-            i++;
-        }
+	// Pass in the objects to merge as arguments.
+	// For a deep extend, set the first argument to `true`.
+	helpers.extend = function () {
 
-        // Merge the object into the extended object
-        var merge = function (obj) {
-            for ( var prop in obj ) {
-                if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
-                    // If deep merge and property is an object, merge properties
-                    if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
-                        extended[prop] = helpers.extend( true, extended[prop], obj[prop] );
-                    } else {
-                        extended[prop] = obj[prop];
-                    }
-                }
-            }
-        };
+		// Variables
+		var extended = {};
+		var deep = false;
+		var i = 0;
+		var length = arguments.length;
 
-        // Loop through each object and conduct a merge
-        for ( ; i < length; i++ ) {
-            var obj = arguments[i];
-            merge(obj);
-        }
+		// Check if a deep merge
+		if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+			deep = arguments[0];
+			i++;
+		}
 
-        return extended;
-    };
+		// Merge the object into the extended object
+		var merge = function (obj) {
+			for ( var prop in obj ) {
+				if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+					// If deep merge and property is an object, merge properties
+					if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+						extended[prop] = helpers.extend( true, extended[prop], obj[prop] );
+					} else {
+						extended[prop] = obj[prop];
+					}
+				}
+			}
+		};
 
-    // Return
-    return {
-        init: methods.init,
-        props: props,
-        elems: elems
-    };
+		// Loop through each object and conduct a merge
+		for ( ; i < length; i++ ) {
+			var obj = arguments[i];
+			merge(obj);
+		}
+
+		return extended;
+	};
+
+	// Return
+	return {
+		init: methods.init,
+		props: props,
+		elems: elems
+	};
 })();
 
 export default siteNavToggle;
