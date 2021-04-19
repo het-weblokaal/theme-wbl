9740dc8d68cfa7289338c691f5a5a0c6ce6bba65
Erik Joling <erik@hetweblokaal.nl>
2021-04-19 15:56:53 +0200

    Improve Template system

--- vendor/het-weblokaal/wbl-theme-foundation/template-system/classes/Template.php
+++ vendor/het-weblokaal/wbl-theme-foundation/template-system/classes/Template.php
@@ -13,12 +13,13 @@ namespace WBL\Theme;
 final class Template {
 
 	/**
-	 * Arguments with which the app can be influenced
+	 * Arguments to customize
 	 *
 	 * @var array
 	 */
 	private static $args = [
-		'template_dir' => 'app/views',
+		'main_template_dir'   => 'vendor/het-weblokaal/wbl-theme-foundation/template-system/views',
+		'custom_template_dir' => 'app/views',
 	];
 
 	/**
@@ -26,14 +27,14 @@ final class Template {
 	 *
 	 * @var string
 	 */
-	private static $template_system_dir = 'het-weblokaal/wbl-theme-foundation/template-system';
+	private static $main_template_dir;
 
 	/**
 	 * Template folder (relative to plugin/theme)
 	 *
 	 * @var string
 	 */
-	private static $template_dir;
+	private static $custom_template_dir;
 
 	/**
      * Constructor method.
@@ -42,77 +43,84 @@ final class Template {
      */
     private function __construct() {}
 
-	/**
-	 * Customize some App properties
-	 *
-	 * @return void
-	 */
-	public static function customize( $args ) {
-		$args = wp_parse_args( $args, static::$args );
 
-		static::$args = $args;
-	}
+	/*=============================================================*/
+	/**                        Getters                             */
+	/*=============================================================*/
+
 
 	/**
-	 * Gets the app template directory
+	 * Gets the main template directory
 	 *
 	 * @return string
 	 */
-	public static function get_template_dir() {
+	private static function get_main_template_dir() {
 
-		if ( is_null(static::$template_dir) ) {
-			static::set_template_dir();
+		// Initialize if it's not set yet
+		if ( is_null(static::$main_template_dir) ) {
+			static::set_main_template_dir();
 		}
 
-		return static::$template_dir;
+		return static::$main_template_dir;
+	}
+
+	/**
+	 * Gets the custom template directory
+	 *
+	 * @return string
+	 */
+	private static function get_custom_template_dir() {
+
+		// Initialize if it's not set yet
+		if ( is_null(static::$custom_template_dir) ) {
+			static::set_custom_template_dir();
+		}
+
+		return static::$custom_template_dir;
 	}
 
 
+	/*=============================================================*/
+	/**                        Setters                             */
+	/*=============================================================*/
+
+
 	/**
-	 * Sets the app template directory
+	 * Sets the main template directory
 	 *
 	 * @return void
 	 */
-	private static function set_template_dir() {
+	private static function set_main_template_dir() {
 
-		// Try to get value from arguments
-		$template_dir = static::$args['template_dir'];
+		// Set the template dir based on class $args
+		$main_template_dir = static::$args['main_template_dir'];
 
 		// Not leading and trailing slashes
-		$template_dir = trim($template_dir, '/');
+		$main_template_dir = trim($main_template_dir, '/');
 
-		static::$template_dir = $template_dir;
-	}
-
-
-	/**
-	 * Get the template relative path
-	 *
-	 * @param string $relative_file relative to the template directory
-	 * @return string relative filepath
-	 */
-	public static function get_template_path( $relative_file = '' ) {
-
-		// Make sure we have a slash at the front of the path.
-		$relative_file = '/' . ltrim( $relative_file, '/' );
-
-		return static::get_template_dir() . $relative_file;
+		static::$main_template_dir = $main_template_dir;
 	}
 
 	/**
-	 * Get the template relative path
+	 * Sets the custom template directory
 	 *
-	 * @param string $relative_file relative to the template directory
-	 * @return string relative filepath
+	 * @return void
 	 */
-	public static function get_template_system_path( $relative_file = '' ) {
+	private static function set_custom_template_dir() {
 
-		// Make sure we have a slash at the front of the path.
-		$relative_file = '/' . ltrim( $relative_file, '/' );
+		// Set the template dir based on class $args
+		$custom_template_dir = static::$args['custom_template_dir'];
 
-		return static::$template_system_dir . $relative_file;
+		// Not leading and trailing slashes
+		$custom_template_dir = trim($custom_template_dir, '/');
+
+		static::$custom_template_dir = $custom_template_dir;
 	}
 
+
+	/*=============================================================*/
+	/**                        General                             */
+	/*=============================================================*/
 
 	/**
 	 * Render template file
@@ -136,20 +144,37 @@ final class Template {
 	 */
 	public static function display( $slug, $name = null, $args = null ) {
 
-		// Allow theme to override arguments
-		$args = apply_filters( "wbl/theme/template/args", $args, $slug, $name);
-		$args = apply_filters( "wbl/theme/template/{$slug}/args", $args, $slug, $name);
+		// Theme::log("display {$slug}");
 
-		Theme::log("wbl/theme/template/{$slug}/args");
+		$templates = array();
+
+		$template_data = [
+			'slug' => $slug,
+			'name' => $name,
+			'args' => $args,
+			'file' => null,
+		];
+
+		// Allow to change the template data
+		$template_data = apply_filters( "wbl/theme/template/{$slug}", $template_data);
+
+		// Set slug, name and args based on filtered template_data
+		$slug = $template_data['slug'];
+		$name = $template_data['name'];
+		$args = $template_data['args'];
 
 		// WordPress Core Action
 		do_action( "get_template_part_{$slug}", $slug, $name, $args );
 
-		$templates = array();
+		// Allow plugin or theme to pass custom file for template
+		if ( !empty($template_data['file']) ) {
+			$templates[] = $template_data['file'];
+		}
 
-		$custom_slug = static::get_template_path( $slug );
-		$vendor_slug = Theme::get_vendor_dir() . '/' . static::get_template_system_path( "views/$slug" );
+		$custom_slug = static::get_custom_template_path( $slug );
+		$vendor_slug = static::get_main_template_path( $slug );
 
+		// Give slugs with name priority in template-order
 		$name = (string) $name;
 		if ( '' !== $name ) {
 		    $templates[] = "{$custom_slug}-{$name}.php";
@@ -167,6 +192,77 @@ final class Template {
 		if ( ! locate_template( $templates, true, false, $args ) ) {
 		    return false;
     	}
+	}
+
+
+	/*=============================================================*/
+	/**                       Utilities                            */
+	/*=============================================================*/
+
+
+	/**
+	 * Get the main template path
+	 *
+	 * @param string $relative_file relative to the template directory
+	 * @return string relative filepath
+	 */
+	public static function get_main_template_path( $relative_file = '' ) {
+
+		// Remove any leading and trailing slashes
+		$relative_file = trim( $relative_file, '/' );
+
+		return static::get_main_template_dir() . '/' . $relative_file;
+	}
+
+	/**
+	 * Get the template relative path
+	 *
+	 * @param string $relative_file relative to the template directory
+	 * @return string relative filepath
+	 */
+	public static function get_custom_template_path( $relative_file = '' ) {
+
+		// Remove any leading and trailing slashes
+		$relative_file = trim( $relative_file, '/' );
+
+		return static::get_custom_template_dir() . '/' . $relative_file;
+	}
+
+	/**
+	 * Allow arguments to be customized
+	 *
+	 * @return void
+	 */
+	public static function customize( $custom_args ) {
+
+		// Parse the custom arguments
+		$custom_args = wp_parse_args( $custom_args, static::$args );
+
+		// Set new args
+		static::$args = $custom_args;
+	}
+
+	/**
+	 * Log data to wp-content/debug.log
+	 *
+	 * It doesn't matter if WP_DEBUG is true because I also want to be able
+	 * to log on production environment (which has WP_DEBUG disabled)
+	 */
+	public static function status_log()  {
+
+	    // Set properties
+		Theme::log( '' );
+		Theme::log( '====== START: TEMPLATE STATUS LOG ======' );
+		Theme::log( '' );
+		Theme::log( '' );
+		Theme::log( '   args:      ' );
+		Theme::log( static::$args );
+		Theme::log( '   main_template_dir:        ' . static::get_main_template_dir() );
+		Theme::log( '   custom_template_dir:      ' . static::get_custom_template_dir() );
+		Theme::log( '' );
+		Theme::log( '' );
+		Theme::log( '======= END: TEMPLATE STATUS LOG =======' );
+		Theme::log( '' );
 	}
 }
 
