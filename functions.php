1ca57b798c6511ec7288ac24c7558fde9edc7f93
Erik Joling <erik@hetweblokaal.nl>
2021-05-10 17:49:31 +0200

    Move bootstrapping to functions.php

--- functions.php
+++ functions.php
@@ -10,6 +10,11 @@ namespace WBL\Theme;
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
+
+# ------------------------------------------------------------------------------
+# Load Theme Foundation
+# ------------------------------------------------------------------------------
+
 // Load the Theme Foundation
 // require_once( __DIR__ . '/vendor/het-weblokaal/wbl-theme-foundation/bootstrap.php' );
 require_once( __DIR__ . '/../wbl-theme-foundation/bootstrap.php' );
@@ -24,5 +29,46 @@ Theme::customize( [
 // Customize Template class
 Template::customize(['main_template_dir' => '../wbl-theme-foundation/template/views']);
 
-// Bootstrap
-require_once( Theme::get_app_path( 'bootstrap.php' ) );
+
+# ------------------------------------------------------------------------------
+# Load Theme Dependencies
+# ------------------------------------------------------------------------------
+
+/**
+ * Composer Dependancies
+ */
+if ( file_exists( Theme::get_vendor_path( 'autoload.php' ) ) ) {
+	require_once( Theme::get_vendor_path( 'autoload.php' ) );
+}
+
+/**
+ * Webfont Loader by WordPress Themes Theme
+ *
+ * @link https://make.wordpress.org/themes/2020/09/25/new-package-to-allow-locally-hosting-webfonts/
+ */
+if ( file_exists( Theme::get_vendor_path( 'wptt-webfont-loader.php' ) ) ) {
+	require_once( Theme::get_vendor_path( 'wptt-webfont-loader.php' ) );
+}
+
+
+# ------------------------------------------------------------------------------
+# Load 'setup' files.
+# ------------------------------------------------------------------------------
+
+array_map( function( $file ) {
+	require_once( Theme::get_app_path( "{$file}.php" ) );
+}, [
+	'setup',
+] );
+
+# ------------------------------------------------------------------------------
+# Load 'function' files.
+# ------------------------------------------------------------------------------
+
+array_map( function( $file ) {
+	require_once( Theme::get_app_path( "{$file}.php" ) );
+}, [
+	'functions',
+] );
+
+
