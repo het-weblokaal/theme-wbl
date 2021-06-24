128c3ae2fbb41a02fa03ce59c243a46bd3d6e41d
Erik Joling <erik@hetweblokaal.nl>
2021-06-24 12:19:23 +0200

    Add project-specs block pattern and column layout styles

--- app/block-editor.php
+++ app/block-editor.php
@@ -11,23 +11,6 @@ namespace WBL\Theme;
  */
 add_action( 'after_setup_theme', function() {
 
-	// Add block category for this theme
-	// add_filter( 'block_categories', __NAMESPACE__ . '\register_block_category' );
-
-	/**
-	 * Register block patterns
-	 */
-
-	// Setup custom category for block patterns
-	register_block_pattern_category( App::get_id(), [ 'label' => App::get_name() . ' blocks' ] );
-
-	// Setup block patterns
-	register_block_pattern( App::get_id() . "/test", [ 
-		'title'      => esc_html__( 'Test', 'wbl-theme' ),
-		'categories' => [ App::get_id() ],
-		'content'    => Template::render( 'block-patterns/test', null )
-	] );
-
 	/**
 	 * Restrict the allowed blocks (opinionated)
 	 */
@@ -39,6 +22,71 @@ add_action( 'after_setup_theme', function() {
 		return $allowed_block_types;
 		
 	}, 10, 2 );
+
+	// Block styles
+	add_action( 'init', function() {
+
+		// Columns: 2
+		register_block_style(
+			'core/columns',
+			array(
+				'name'  => 'columns-2',
+				'label' => esc_html__( 'Twee kolommen op een rij', 'wbl-theme' ),
+				'isDefault' => true
+			)
+		);
+
+		// Columns: 3
+		register_block_style(
+			'core/columns',
+			array(
+				'name'  => 'columns-3',
+				'label' => esc_html__( 'Drie kolommen op een rij', 'wbl-theme' ),
+			)
+		);
+
+		// Columns: 4
+		register_block_style(
+			'core/columns',
+			array(
+				'name'  => 'columns-4',
+				'label' => esc_html__( 'Vier kolommen op een rij', 'wbl-theme' ),
+			)
+		);
+
+		// List: no-bullets
+		register_block_style(
+			'core/list',
+			array(
+				'name'  => 'no-bullets',
+				'label' => esc_html__( 'Een lijst zonder punten', 'wbl-theme' ),
+			)
+		);
+	} );
+
+	// Add block category for this theme
+	// add_filter( 'block_categories', __NAMESPACE__ . '\register_block_category' );
+
+	/**
+	 * Register block patterns
+	 */
+
+	// Setup custom category for block patterns
+	register_block_pattern_category( App::get_id(), [ 'label' => App::get_name() . ' blocks' ] );
+
+	// Setup Test block pattern
+	register_block_pattern( App::get_id() . "/test", [ 
+		'title'      => esc_html__( 'Test', 'wbl-theme' ),
+		'categories' => [ App::get_id() ],
+		'content'    => Template::render( 'block-patterns/test', null )
+	] );
+
+	// Setup Project Specs block pattern
+	register_block_pattern( App::get_id() . "/project-specs", [ 
+		'title'      => esc_html__( 'Project Specs', 'wbl-theme' ),
+		'categories' => [ App::get_id() ],
+		'content'    => Template::render( 'block-patterns/project-specs', null )
+	] );
 
 }, 5 );
 
---
+++ templates/block-patterns/project-specs.php
@@ -0,0 +1,76 @@
+<?php
+namespace WBL\Theme;
+?>
+<!-- wp:group {"align":"wide"} -->
+<div class="wp-block-group alignwide"><div class="wp-block-group__inner-container"><!-- wp:columns {"className":"is-style-columns-4"} -->
+<div class="wp-block-columns is-style-columns-4"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>Opdrachtgever</h3>
+<!-- /wp:heading -->
+
+<!-- wp:image {"id":10,"width":150,"height":100,"sizeSlug":"thumbnail","linkDestination":"none"} -->
+<figure class="wp-block-image size-thumbnail is-resized"><img src="http://hetweblokaal.test/wp-content/uploads/2021/04/P1190337-300x200.jpg" alt="P1190337" class="wp-image-10" width="150" height="100"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>Website</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><a href="http://natuurlijkeveehouderij.nl/" data-type="URL" data-id="natuurlijkeveehouderij.nl/">natuurlijkeveehouderij.nl</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>Oplevering</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>2021</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3} -->
+<h3>Werkzaamheden</h3>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"is-style-no-bullets"} -->
+<ul class="is-style-no-bullets"><li>Projectmanagement</li><li>Onderzoek</li><li>Ontwerp</li><li>Bouw</li><li>Training</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:heading {"level":3} -->
+<h3>Specificaties</h3>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"className":"is-style-columns-4"} -->
+<div class="wp-block-columns is-style-columns-4"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
+<ul class="is-style-no-bullets"><li>WordPress</li><li>Gutenberg (?)</li><li>Responsive</li><li>SEO</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
+<ul class="is-style-no-bullets"><li>GPDR / AVG</li><li>Toegankelijkheid</li><li>Statistieken</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
+<ul class="is-style-no-bullets"><li>Meertalig</li><li>Nieuws</li><li>Agenda</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
+<ul class="is-style-no-bullets"><li>Portfolio</li><li>Nieuwsbrief </li><li>Social Media</li></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:group -->
