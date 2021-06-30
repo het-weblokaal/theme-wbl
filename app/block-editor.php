6b2097be4a8b534e0253ec12cddf190d2047d09d
Erik Joling <erik@hetweblokaal.nl>
2021-06-30 10:57:23 +0200

    Improve block-editor setup and add renders for archive and posts block

--- app/block-editor.php
+++ app/block-editor.php
@@ -17,66 +17,48 @@ add_action( 'after_setup_theme', function() {
 	add_filter( 'allowed_block_types', function( $allowed_block_types ) {
 
 		// Add blocks specifically for this theme
-		$allowed_block_types[] = 'wbl-projects/projects';
-		$allowed_block_types[] = 'wbl-blocks/archive-loop';
 		$allowed_block_types[] = 'core/gallery';
 
 		return $allowed_block_types;
 		
 	}, 10, 2 );
 
-	// Block styles
-	add_action( 'init', function() {
 
-		// Columns: 2
-		register_block_style(
-			'core/columns',
-			array(
-				'name'  => 'columns-2',
-				'label' => esc_html__( 'Twee kolommen op een rij', 'wbl-theme' ),
-				'isDefault' => true
-			)
-		);
+}, 5 );
 
-		// Columns: 3
-		register_block_style(
-			'core/columns',
-			array(
-				'name'  => 'columns-3',
-				'label' => esc_html__( 'Drie kolommen op een rij', 'wbl-theme' ),
-			)
-		);
+// Block styles
+add_action( 'init', function() {
 
-		// Columns: 4
-		register_block_style(
-			'core/columns',
-			array(
-				'name'  => 'columns-4',
-				'label' => esc_html__( 'Vier kolommen op een rij', 'wbl-theme' ),
-			)
-		);
+	// Columns: 2
+	register_block_style( 'core/columns', array(
+		'name'  => 'columns-2',
+		'label' => esc_html__( 'Twee kolommen op een rij', 'wbl-theme' ),
+		'isDefault' => true
+	) );
 
-		// List: no-bullets
-		register_block_style(
-			'core/list',
-			array(
-				'name'  => 'no-bullets',
-				'label' => esc_html__( 'Een lijst zonder punten', 'wbl-theme' ),
-			)
-		);
+	// Columns: 3
+	register_block_style( 'core/columns', array(
+		'name'  => 'columns-3',
+		'label' => esc_html__( 'Drie kolommen op een rij', 'wbl-theme' ),
+	) );
 
-		// Gallery: inline image
-		register_block_style(
-			'core/gallery',
-			array(
-				'name'  => 'inline-images',
-				'label' => esc_html__( 'Galerij met inline afbeeldingen', 'wbl-theme' ),
-			)
-		);
-	} );
+	// Columns: 4
+	register_block_style( 'core/columns', array(
+		'name'  => 'columns-4',
+		'label' => esc_html__( 'Vier kolommen op een rij', 'wbl-theme' ),
+	) );
 
-	// Add block category for this theme
-	// add_filter( 'block_categories', __NAMESPACE__ . '\register_block_category' );
+	// List: no-bullets
+	register_block_style( 'core/list', array(
+		'name'  => 'no-bullets',
+		'label' => esc_html__( 'Een lijst zonder punten', 'wbl-theme' ),
+	) );
+
+	// Gallery: inline image
+	register_block_style( 'core/gallery', array(
+		'name'  => 'inline-images',
+		'label' => esc_html__( 'Galerij met inline afbeeldingen', 'wbl-theme' ),
+	) );
 
 	/**
 	 * Register block patterns
@@ -105,29 +87,6 @@ add_action( 'after_setup_theme', function() {
 		'categories' => [ App::get_id() ],
 		'content'    => Template::render( 'block-patterns/project-specs', null )
 	] );
+} );
 
-}, 5 );
 
-/**
- * Creating a new block category.
- *
- * @param   array $categories     List of block categories.
- * @return  array
- */
-function register_block_category( $categories ) {
-
-    // The block category title and slug.
-    $block_category = [
-		'title' => App::get_name(),
-		'slug' => App::get_id(),
-		'icon'  => 'star-filled'
-    ];
-
-    $category_slugs = wp_list_pluck( $categories, 'slug' );
-
-    if ( ! in_array( $block_category['slug'], $category_slugs, true ) ) {
-        $categories = array_merge( $categories, [ $block_category ] );
-    }
-
-    return $categories;
-}
--- app/general.php
+++ app/general.php
@@ -34,6 +34,12 @@ add_action( 'after_setup_theme', function() {
 
 	add_filter( 'body_class', __NAMESPACE__ . '\add_page_image_class', 11, 2 );
 
+	// Render archive loop block
+	add_filter( 'wbl/blocks/archive-loop/render', __NAMESPACE__ . '\render_archive_loop_block', 10, 2 );
+
+	// Render posts block
+	add_filter( 'wbl/blocks/posts/render', __NAMESPACE__ . '\render_posts_block', 10, 2 );
+
 }, 5 );
 
 /**
@@ -53,4 +59,57 @@ function add_page_image_class( $classes ) {
 	}
 
 	return $classes;
+}
+
+/**
+ * Render the archive loop through a block
+ *
+ * @return  string
+ */
+function render_archive_loop_block( $render, $attributes ) {
+
+	if ( ! is_post_type_archive() ) {
+		return false;
+	}
+
+	if ( get_post_type_on_archive() == 'wbl_project' ) {
+		$render = Template::render( 'loop/grid', null );
+	}
+	else {
+		$render = Template::render( 'loop/blog', null );
+	}	
+
+	return $render;
+}
+
+/**
+ * Render the posts through a block
+ *
+ * @return  string
+ */
+function render_posts_block( $render, $attributes ) {
+
+	$query_args = [
+		'posts_per_page'   => $attributes['postsToShow'],
+		'post_type'        => 'post',
+		// 'post_status'      => 'publish',
+		// 'order'            => $attributes['order'],
+		// 'orderby'          => $attributes['orderBy'],
+		// 'suppress_filters' => false,
+	];
+
+	// Extra classes
+	$extra_classes = [];
+
+	// Alignment
+	if (! empty($args['align'])) {
+		$extra_classes[] = 'align' . $args['align'];
+	}
+
+	$render = Template::render( 'loop/blog', null, [
+		'query_args' => $query_args,
+		'extra_classes' => $extra_classes
+	] );
+
+	return $render;
 }
