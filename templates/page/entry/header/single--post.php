d2d3c562d6a482de18c8d13bc40f24c652f0eafd
Erik Joling <erik@hetweblokaal.nl>
2021-07-07 09:21:18 +0200

    Add post and project label for single templates

--- templates/page/entry/header/single--post.php
+++ templates/page/entry/header/single--post.php
@@ -1,16 +1,13 @@
-<?php
-
-namespace WBL\Theme;
-
-?>
 <header class="page__header entry__header">
 
-	<h1 class="page__title entry__title"><?= get_page_title() ?></h1>
+	<div class="page__label entry__label"><?= WBL\Theme\get_post_type_archive_link() ?></div>
+
+	<h1 class="page__title entry__title"><?= WBL\Theme\get_page_title() ?></h1>
 
 	<div class="page__meta entry__meta">
 
-		<?php Template::display( 'components/entry-author', null, [ 'label' => 'door' ] ) ?>
-		<?php Template::display( 'components/entry-date' ) ?>
+		<?php WBL\Theme\Template::display( 'components/entry-author', null, [ 'label' => 'door' ] ) ?>
+		<?php WBL\Theme\Template::display( 'components/entry-date' ) ?>
 	</div>
 
 </header>
--- templates/page/entry/header/single--wbl_project.php
+++ templates/page/entry/header/single--wbl_project.php
@@ -1,14 +1,11 @@
-<?php
-
-namespace WBL\Theme;
-
-?>
 <header class="page__header entry__header">
 
-	<h1 class="page__title entry__title"><?= get_page_title() ?></h1>
+	<div class="page__label entry__label"><?= WBL\Theme\get_post_type_archive_link() ?></div>
+
+	<h1 class="page__title entry__title"><?= WBL\Theme\get_page_title() ?></h1>
 
 	<div class="page__meta entry__meta">
-		<?php echo render_entry_terms( [ 'taxonomy' => 'project_category', 'link' => false, 'sep' => '' ] ); ?>
+		<?php echo WBL\Theme\render_entry_terms( [ 'taxonomy' => 'project_category', 'link' => false, 'sep' => '' ] ); ?>
 	</div>
 
 </header>
