8f24eca7896c4d62c79efe03d4ab4e71fc2a39f7
Erik Joling <erik@hetweblokaal.nl>
2021-07-12 16:54:10 +0200

    Fix and tidy projects specs block pattern

--- templates/block-patterns/project-specs.php
+++ templates/block-patterns/project-specs.php
@@ -2,75 +2,119 @@
 namespace WBL\Theme;
 ?>
 <!-- wp:group {"align":"full", "className":"project-specs"} -->
-<div class="wp-block-group project-specs alignfull"><div class="wp-block-group__inner-container"><!-- wp:columns {"className":"is-style-columns-4"} -->
-<div class="wp-block-columns is-style-columns-4"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3} -->
-<h3>Opdrachtgever</h3>
-<!-- /wp:heading -->
+<div class="wp-block-group project-specs alignfull"><div class="wp-block-group__inner-container">
 
-<!-- wp:image {sizeSlug":"thumbnail"} -->
-<figure class="wp-block-image size-thumbnail"><img src="<?= App::asset('img/placeholder.png') ?>" alt=""/></figure>
-<!-- /wp:image -->
-<!-- /wp:column -->
+	<!-- wp:columns {"className":"is-style-columns-4"} -->
+	<div class="wp-block-columns is-style-columns-4">
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3} -->
-<h3>Website</h3>
-<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
 
-<!-- wp:paragraph -->
-<p><a href="http://domeinnaam.nl/" data-type="URL" data-id="domeinnaam.nl/">domeinnaam.nl</a></p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+			<!-- wp:heading {"level":3} -->
+			<h3>Opdrachtgever</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3} -->
-<h3>Oplevering</h3>
-<!-- /wp:heading -->
+			<!-- wp:image {sizeSlug":"thumbnail"} -->
+			<figure class="wp-block-image size-thumbnail"><img src="<?= App::asset('img/placeholder.png') ?>" alt=""/></figure>
+			<!-- /wp:image -->
 
-<!-- wp:paragraph -->
-<p>2021</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
+		</div>
+		<!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":3} -->
-<h3>Werkzaamheden</h3>
-<!-- /wp:heading -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
 
-<!-- wp:list {"className":"is-style-no-bullets"} -->
-<ul class="is-style-no-bullets"><li>Projectmanagement</li><li>Onderzoek</li><li>Ontwerp</li><li>Bouw</li><li>Training</li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns -->
+			<!-- wp:heading {"level":3} -->
+			<h3>Website</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:heading {"level":3} -->
-<h3>Specificaties</h3>
-<!-- /wp:heading -->
+			<!-- wp:paragraph -->
+			<p><a href="http://domeinnaam.nl/" data-type="URL" data-id="domeinnaam.nl/">domeinnaam.nl</a></p>
+			<!-- /wp:paragraph -->
 
-<!-- wp:columns {"className":"is-style-columns-4"} -->
-<div class="wp-block-columns is-style-columns-4"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
-<ul class="is-style-no-bullets"><li>WordPress</li><li>Gutenberg (?)</li><li>Responsive</li><li>SEO</li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
+		</div>
+		<!-- /wp:column -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
-<ul class="is-style-no-bullets"><li>GPDR / AVG</li><li>Toegankelijkheid</li><li>Statistieken</li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
+		<!-- wp:column -->
+		<div class="wp-block-column">
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
-<ul class="is-style-no-bullets"><li>Meertalig</li><li>Nieuws</li><li>Agenda</li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
+			<!-- wp:heading {"level":3} -->
+			<h3>Oplevering</h3>
+			<!-- /wp:heading -->
 
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:list {"className":"is-style-no-bullets"} -->
-<ul class="is-style-no-bullets"><li>Portfolio</li><li>Nieuwsbrief </li><li>Social Media</li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div></div>
+			<!-- wp:paragraph -->
+			<p>2021</p>
+			<!-- /wp:paragraph -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:heading {"level":3} -->
+			<h3>Werkzaamheden</h3>
+			<!-- /wp:heading -->
+
+			<!-- wp:list {"className":"is-style-no-bullets"} -->
+			<ul class="is-style-no-bullets"><li>Projectmanagement</li><li>Onderzoek</li><li>Ontwerp</li><li>Bouw</li><li>Training</li></ul>
+			<!-- /wp:list -->
+
+		</div>
+		<!-- /wp:column -->
+
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:heading {"level":3} -->
+	<h3>Specificaties</h3>
+	<!-- /wp:heading -->
+
+	<!-- wp:columns {"className":"is-style-columns-4"} -->
+	<div class="wp-block-columns is-style-columns-4">
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:list {"className":"is-style-no-bullets"} -->
+			<ul class="is-style-no-bullets"><li>WordPress</li><li>Gutenberg (?)</li><li>Responsive</li><li>SEO</li></ul>
+			<!-- /wp:list -->
+
+		</div>		
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:list {"className":"is-style-no-bullets"} -->
+			<ul class="is-style-no-bullets"><li>GPDR / AVG</li><li>Toegankelijkheid</li><li>Statistieken</li></ul>
+			<!-- /wp:list -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+			<!-- wp:list {"className":"is-style-no-bullets"} -->
+			<ul class="is-style-no-bullets"><li>Meertalig</li><li>Nieuws</li><li>Agenda</li></ul>
+			<!-- /wp:list -->
+
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+
+		<!-- wp:list {"className":"is-style-no-bullets"} -->
+		<ul class="is-style-no-bullets"><li>Portfolio</li><li>Nieuwsbrief </li><li>Social Media</li></ul>
+		<!-- /wp:list -->
+
+		</div>
+		<!-- /wp:column -->
+
+	</div>
+	<!-- /wp:columns -->
+
+</div></div>
 <!-- /wp:group -->
